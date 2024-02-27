8f90372a6509d1fdfb7e38a4ad2553191f5e73c9
Kerry Wang <kerrywang369@gmail.com>
2024-02-27 11:12:49 -0700

    minors

--- main.js
+++ main.js
@@ -1,73 +1,74 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require("puppeteer");
 
 const getDeals = async () => {
-    const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
 
-    let deals = []
+  let deals = [];
 
-    function* generateURLs() {
-        yield `https://forums.redflagdeals.com/hot-deals-f9/?sk=tt&rfd_sk=tt&sd=d`;
-        for (let page_num = 1; page_num < 5; ++page_num) {
-            yield `https://forums.redflagdeals.com/hot-deals-f9/${page_num}/?sk=tt&rfd_sk=tt`;
+  function* generateURLs() {
+    yield `https://forums.redflagdeals.com/hot-deals-f9/?sk=tt&rfd_sk=tt&sd=d`;
+    for (let page_num = 1; page_num < 5; ++page_num) {
+      yield `https://forums.redflagdeals.com/hot-deals-f9/${page_num}/?sk=tt&rfd_sk=tt`;
+    }
+  }
+
+  for (let url of generateURLs()) {
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const page_deals = await page.evaluate(() => {
+      const entries = document.querySelectorAll("li.row.topic");
+
+      var ret = [];
+
+      for (entry of entries) {
+        const thread_id = entry.getAttribute("data-thread-id");
+        const title = entry.querySelector(".topic_title_link").innerText;
+        const deal_url =
+          "https://forums.redflagdeals.com/" +
+          entry.querySelector(".topic_title_link").getAttribute("href");
+        const post_time = entry.querySelector(".first-post-time").innerText;
+        const description_list = entry.querySelector(".post_voting");
+        let votes = NaN;
+        if (description_list) {
+          votes = Number(description_list.getAttribute("data-total"));
         }
-    }
+        const deal = { thread_id, title, votes, post_time, deal_url };
 
-    for (url of generateURLs()) {
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        const page_deals = await page.evaluate(() => {
-            const entries = document.querySelectorAll("li.row.topic");
-
-            var ret = []
-
-            for (entry of entries) {
-                const thread_id = entry.getAttribute("data-thread-id")
-                const title = entry.querySelector(".topic_title_link").innerText;
-                const deal_url = entry.querySelector(".topic_title_link").getAttribute("href");
-                const post_time = entry.querySelector(".first-post-time").innerText;
-                const description_list = entry.querySelector(".post_voting");
-                let votes = NaN;
-                if (description_list) {
-                    votes = Number(description_list.getAttribute("data-total"));
-                }
-                const deal = { thread_id, title, votes, post_time, deal_url };
-
-                function filter(deal) {
-                    if (deal.votes != NaN && deal.votes > 10) {
-                        return true;
-                    }
-                    return false;
-                }
-
-                if (filter(deal)) {
-                    ret.push(deal);
-                }
-            }
-
-            return ret;
-        });
-
-        deals = deals.concat(page_deals);
-    }
-
-    // Remove duplicate thread_ids (new deal during scrapping pushing deal to next page)
-    let visited = {}
-    for (let deal of deals) {
-        if (deal.thread_id in visited) {
-            visited[deal.thread_id] += 1;
-        } else {
-            visited[deal.thread_id] = 1;
+        function filter(deal) {
+          if (deal.votes != NaN && deal.votes > 10) {
+            return true;
+          }
+          return false;
         }
+
+        if (filter(deal)) {
+          ret.push(deal);
+        }
+      }
+
+      return ret;
+    });
+
+    deals = deals.concat(page_deals);
+  }
+
+  // Remove duplicate thread_ids (new deal during scrapping pushing deal to next page)
+  let visited = {};
+  for (let deal of deals) {
+    if (deal.thread_id in visited) {
+      visited[deal.thread_id] += 1;
+    } else {
+      visited[deal.thread_id] = 1;
     }
-    deals = deals.filter((deal) => {
-        return visited[deal.thread_id] == 1
-    })
+  }
+  deals = deals.filter((deal) => {
+    return visited[deal.thread_id] == 1;
+  });
 
-    console.log(deals);
+  await browser.close();
 
-    await browser.close();
+  return deals;
 };
 
-getDeals();
+getDeals().then((res) => console.log(res));
--- raw.js
+++ raw.js
@@ -4,8 +4,6 @@ const puppeteer = require('puppeteer');
 const getDeals = async () => {
     const browser = await puppeteer.launch();
 
-    let deals = []
-
     function* generateURLs() {
         yield `https://forums.redflagdeals.com/hot-deals-f9/?sk=tt&rfd_sk=tt&sd=d`;
         for (let page_num = 1; page_num < 5; ++page_num) {
@@ -13,7 +11,7 @@ const getDeals = async () => {
         }
     }
 
-    for (url of generateURLs()) {
+    for (let url of generateURLs()) {
         const page = await browser.newPage();
         await page.goto(url);
         const content = await page.content();
